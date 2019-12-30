5a1ed1611dc994e634223f5f820118a9dee596f9
sekonioluwaseun@gmail.com <sekonioluwaseun@gmail.com>
2019-12-30 02:44:08 +0100

    Updates

---
+++ precache-manifest.cb4ebc1f00f87b17381ab2deb21f524d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f47168e842a7a6faa9d4583de1f8222",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bacdde3f6cd52f8dd01f",
+    "url": "/monster-rolodex/static/css/main.77d6d3b6.chunk.css"
+  },
+  {
+    "revision": "04074e2a822987c35bc5",
+    "url": "/monster-rolodex/static/js/2.64580605.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.64580605.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bacdde3f6cd52f8dd01f",
+    "url": "/monster-rolodex/static/js/main.503bab93.chunk.js"
+  },
+  {
+    "revision": "df1528df8965a3cc0f7e",
+    "url": "/monster-rolodex/static/js/runtime-main.cb0890c9.js"
+  }
+]);
